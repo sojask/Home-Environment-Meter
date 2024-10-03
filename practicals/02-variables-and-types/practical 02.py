cfa710e24258a962e632bd9fcf98caa5684ab9be
sojask <TheDavidAmata@gmail.com>
2024-10-03 11:14:34 +0100

    up to "esc sequences" practical 02.py

--- practicals/02-variables-and-types/practical 02.py
+++ practicals/02-variables-and-types/practical 02.py
@@ -1,1 +1,35 @@
-print("Hello World")
+total = 100
+print("The total is now", total)
+
+total = total + 99
+print("The total is now", total)
+total += 99
+print("The total is now", total)
+
+total = total - 1
+print("The total is", total)
+total = total * 4
+print("The total is", total)
+total = total / 2
+print("The total is", total)
+
+total = 98.2
+count = 5
+average = total / count
+print("The average is", average)
+
+name = "sojask"
+address = "55 ABC Avenue"
+number = "07944371304"
+print(name)
+print(address)
+print(number)
+print("The length of name is", len(name))
+
+age = int(input("Enter your age "))
+print("in one year your age will be", age + 1)
+
+value1 = int(input("Enter a value "))
+value2 = int(input("Enter another value "))
+product = value1 * value2
+print("The product is", product)
