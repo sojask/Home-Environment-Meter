44f06f0892b8b8998d5b81294557a668c7ae271f
sojask <TheDavidAmata@gmail.com>
2024-10-10 11:20:40 +0100

    added age and count program files

--- practicals/03-control-statements/count.py
+++ practicals/03-control-statements/count.py
@@ -5,3 +5,25 @@ while count > 0:
 
 for n in range(20, 0, -1):
     print(n, "doubled is", n * 2)
+
+for n in range(2, 12, 2):
+    print(n, "to the power of", n, "=", n ** n)
+
+value = int(input("enter a number: "))
+for n in range(2, value//2):
+    if value % n == 0:
+        print(value, "is not a prime number")
+        break
+    else:
+        print(value, "is a prime number")
+
+grades = [20, 50, 43, 33, 90, 15]
+pass_mark = 40
+passes = 0
+total = 0
+for grade in grades:
+    if grade < pass_mark:
+        continue
+    passes += 1
+    total += grade
+print("average pass mark was", total/passes)
